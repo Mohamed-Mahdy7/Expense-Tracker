c858031c04b7ac4eed7cad39d599faf260c64ebf
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2025-12-27 11:09:45 +0200

    added static files

--- main/dashboard.py
+++ main/dashboard.py
@@ -1,6 +1,6 @@
 """Contain dashbord logic"""
 from datetime import datetime
-from sqlalchemy import text, func
+from sqlalchemy import Integer, cast, text, func
 from flask import current_app, render_template, request
 from flask_jwt_extended import get_jwt_identity
 
@@ -10,18 +10,21 @@ from . import db
 
 def dashboard():
     user_id = int(get_jwt_identity())
-    
-    now = datetime.now()
-    month = request.args.get("month", f"{now.month:02}")
-    year = request.args.get("year", str(now.year))
+    month_str = request.args.get("month")
+    if month_str:
+        year, month = map(int, month_str.split("-"))
+    else:
+        now = datetime.now()
+        month = request.args.get("month", f"{now.month:02}")
+        year = request.args.get("year", str(now.year))
     
     #Total income
     income = db.session.query(db.func.coalesce(
         db.func.sum(Transactions.amount), 0)).filter(
         Transactions.user_id==user_id,
         Transactions.transaction_type=="income",
-        db.extract("month", Transactions.date)==month,
-        db.extract("year", Transactions.date)==year,
+        cast(func.strftime('%Y', Transactions.date), Integer) == year,
+        cast(func.strftime('%m', Transactions.date), Integer) == month,
         ).scalar()
     
     #Total expenses
@@ -29,8 +32,8 @@ def dashboard():
         db.func.sum(Transactions.amount), 0)).filter(
         Transactions.user_id==user_id,
         Transactions.transaction_type=="expense",
-        db.extract("month", Transactions.date)==month,
-        db.extract("year", Transactions.date)==year,
+        cast(func.strftime('%m', Transactions.date), Integer) == month,
+        cast(func.strftime('%Y', Transactions.date), Integer) == year,
         ).scalar()
     
     balance = income - expenses
@@ -43,8 +46,8 @@ def dashboard():
         ).join(Transactions, Categories.id==Transactions.category_id).filter(
         Transactions.user_id==user_id,
         Transactions.transaction_type=="expense",
-        db.extract("month", Transactions.date)==month,
-        db.extract("year", Transactions.date)==year,
+        cast(func.strftime('%m', Transactions.date), Integer) == month,
+        cast(func.strftime('%Y', Transactions.date), Integer) == year,
     ).group_by(Categories.name).all()
     
     
@@ -58,6 +61,4 @@ def dashboard():
         balance=balance,
         labels=labels,
         values=values,
-        month=month,
-        year=year
     )
